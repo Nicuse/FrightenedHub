eb80c4e362b24c23bfa1e6eb896be1ed880876c3
Nicuse <92673025+Nicuse@users.noreply.github.com>
2023-02-14 17:02:43 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
---r
+function loadurl(url)
+    loadstring(game:HttpGet(url))()
+end
+repo = 'https://raw.githubusercontent.com/Nicuse/FrightenedHub/main/Games/'
+if game.GameId == 2294168059 then
+    loadurl(repo..'TheMimic.lua')
+end
