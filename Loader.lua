82c1f9bdec454a91c9b57728bade532dc33cd412
Nicuse <92673025+Nicuse@users.noreply.github.com>
2023-02-18 16:22:16 +0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,10 @@
+local GameId = game.GameId
 function loadurl(url)
     loadstring(game:HttpGet(url))()
 end
 repo = 'https://raw.githubusercontent.com/Nicuse/FrightenedHub/main/Games/'
-if game.GameId == 2294168059 then
+if GameId == 2294168059 then
     loadurl(repo..'TheMimic.lua')
+elseif GameId == 3085257211 then
+    loadurl(repo..'RainbowFriends.lua')
 end
